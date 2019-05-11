338c5c7caac56a8b27676b2d3c4673117ff560ea
SteveMustanski <steve.mustanski@gmail.com>
2019-05-11 15:39:24 -0400

    added get all code to get contacts route

--- src/routes/crmRoutes.js
+++ src/routes/crmRoutes.js
@@ -1,17 +1,12 @@
-import { addNewContact } from '../contollers/crmController';
+import { addNewContact, getContacts } from '../contollers/crmController';
 
 const routes = app => {
   app
     .route('/contact')
-    .get(
-      (req, res, next) => {
-        console.log(`${req.method} request from: ${req.originalUrl}`);
-        next();
-      },
-      (req, res, next) => {
-        res.send('GET response to /contact was successfull');
-      },
-    )
+    .get((req, res, next) => {
+      console.log(`${req.method} request from: ${req.originalUrl}`);
+      next();
+    }, getContacts)
     .post(addNewContact);
 
   app
